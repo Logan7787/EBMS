69bd19d64f8e1dbbbd2cd2088c262726845f0b39
Vinodhan <vinodhan1987@gmail.com>
2026-06-01 11:31:17 +0530

    remove capacitor and restore pure vite react web app

--- android/app/src/main/java/com/ebms/app/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.ebms.app;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.ebms.app',
-  appName: 'EBMS',
-  webDir: 'dist'
-};
-
-export default config;
